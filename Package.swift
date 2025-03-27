77f788ab21dc4fa91403a8b89cf38b8ce879a8e1
Zamderax <zamderax@gmail.com>
2025-03-27 11:40:04 -0700

    downgrading swift crypto for windows compat

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apache-edge/cbor.git", from: "0.0.2"),
-        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "4.0.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
